35a7f6c0b10c5163990a354bbbdf15c4537bcb98
Arthur <vashulenko94@gmail.com>
2024-12-17 22:26:15 +0200

    added choosing people

--- lib/features/people/presentation/pages/people_page.dart
+++ lib/features/people/presentation/pages/people_page.dart
@@ -13,6 +13,7 @@ class PeoplePage extends ConsumerStatefulWidget {
 
 class _PeoplePageState extends ConsumerState<PeoplePage> {
   final ScrollController _scrollController = ScrollController();
+  int? _selectedIndex;
 
   @override
   void initState() {
@@ -28,6 +29,27 @@ class _PeoplePageState extends ConsumerState<PeoplePage> {
         _scrollController.position.maxScrollExtent - 200) {
       ref.read(paginatedPeopleProvider.notifier).fetchNextPage();
     }
+  }
+
+  void _onCardTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    const cardWidth = 116.0;
+    final targetOffset = (index * cardWidth) -
+        (MediaQuery.of(context).size.width / 2 - cardWidth / 2);
+
+    final currentOffset = _scrollController.offset;
+    if ((currentOffset - targetOffset).abs() < 10.0) {
+      return;
+    }
+
+    _scrollController.animateTo(
+      targetOffset,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
   }
 
   @override
@@ -52,7 +74,15 @@ class _PeoplePageState extends ConsumerState<PeoplePage> {
                 itemCount: peopleList.length,
                 itemBuilder: (context, index) {
                   final person = peopleList[index];
-                  return _PeopleCard(person: person);
+                  final isSelected = index == _selectedIndex;
+
+                  return GestureDetector(
+                    onTap: () => _onCardTap(index),
+                    child: _PeopleCard(
+                      person: person,
+                      isSelected: isSelected,
+                    ),
+                  );
                 },
               ),
       ),
@@ -62,39 +92,50 @@ class _PeoplePageState extends ConsumerState<PeoplePage> {
 
 class _PeopleCard extends StatelessWidget {
   final PeopleEntity person;
+  final bool isSelected;
 
-  const _PeopleCard({required this.person});
+  const _PeopleCard({required this.person, this.isSelected = false});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(
-              'https://image.tmdb.org/t/p/w200${person.image}',
-              width: 100,
-              height: 100,
-              fit: BoxFit.cover,
-              errorBuilder: (_, __, ___) =>
-                  const Icon(Icons.person, size: 100, color: Colors.grey),
+      child: Container(
+        width: 100,
+        decoration: BoxDecoration(
+          border: isSelected
+              ? Border.all(color: const Color(0xff032541), width: 2.0)
+              : null,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.network(
+                'https://image.tmdb.org/t/p/w200${person.image}',
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+                errorBuilder: (_, __, ___) =>
+                    const Icon(Icons.person, size: 100, color: Colors.grey),
+              ),
             ),
-          ),
-          const SizedBox(height: 8.0),
-          Text(
-            person.name,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0),
-          ),
-          Text(
-            'Popularity: ${person.popularity}',
-            style: const TextStyle(fontSize: 10.0, color: Colors.grey),
-          ),
-        ],
+            const SizedBox(height: 8.0),
+            Text(
+              person.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style:
+                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0),
+            ),
+            Text(
+              '${person.popularity}',
+              style: const TextStyle(fontSize: 10.0, color: Colors.grey),
+            ),
+          ],
+        ),
       ),
     );
   }
